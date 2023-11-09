9a20e8b9faf276100249fd3b2671459543d2f4e9
Halal1122 <mdhealaluddin505@gmail.com>
2023-11-09 07:09:30 +0600

    wishlist delete and deltails btn added and worked

--- src/Pages/Wishlist.jsx
+++ src/Pages/Wishlist.jsx
@@ -24,7 +24,7 @@ const Wishlist = () => {
         const proceed =
             Swal.fire({
                 title: "Are you sure?",
-                text: "You won't be able to revert this!",
+                text: "You won't be able to revert this",
                 icon: "warning",
                 showCancelButton: true,
                 confirmButtonColor: "#3085d6",
